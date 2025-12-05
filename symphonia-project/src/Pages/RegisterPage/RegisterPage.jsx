81c5b41c8164da1d9384116a247ec1a1eb5edb9f
PedroBarbosaIF <pedro.barbosa40@aluno.ifce.edu.br>
2025-12-05 16:42:37 -0300

    estilização tela de cadastro

--- symphonia-project/src/Pages/RegisterPage/RegisterPage.jsx
+++ symphonia-project/src/Pages/RegisterPage/RegisterPage.jsx
@@ -2,50 +2,93 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
 
+import logo from "../../assets/logo.png";
+
+import "./style.css";
+
 export default function RegisterPage() {
-   const navigate = useNavigate();
-   const { register } = useAuth();
-   const [username, setUsername] = useState("");
-   const [password, setPassword] = useState("");
-   const [confirm, setConfirm] = useState("");
-   const [error, setError] = useState(null);
+    const navigate = useNavigate();
+    const { register } = useAuth();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirm, setConfirm] = useState("");
+    const [error, setError] = useState(null);
 
-   async function handleSubmit(e) {
-      e.preventDefault();
-      setError(null);
-      if (password !== confirm) {
-         setError('Senhas não conferem');
-         return;
-      }
-      const res = await register({ name: username, password });
-      if (res.ok) {
-         navigate("/home");
-      } else {
-         setError(res.message || "Falha no registro");
-      }
-   }
+    async function handleSubmit(e) {
+        e.preventDefault();
+        setError(null);
+        if (password !== confirm) {
+            setError("Senhas não conferem");
+            return;
+        }
+        const res = await register({ name: username, password });
+        if (res.ok) {
+            navigate("/home");
+        } else {
+            setError(res.message || "Falha no registro");
+        }
+    }
 
-   return (
-      <div>
-         <h1>Tela de cadastro</h1>
-         <form onSubmit={handleSubmit}>
-            <div>
-               <label>Nome de usuário</label>
-               <input value={username} onChange={e => setUsername(e.target.value)} />
-            </div>
-            <div>
-               <label>Senha</label>
-               <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-            </div>
-            <div>
-               <label>Confirmar Senha</label>
-               <input type="password" value={confirm} onChange={e => setConfirm(e.target.value)} />
-            </div>
-            {error && <div style={{ color: "red" }}>{error}</div>}
-            <button type="submit">Criar conta</button>
-            <button type="button" onClick={() => navigate(-1)}>Voltar</button>
-         </form>
-      </div>
-   );
+    return (
+        <>
+            {/*<!-- header -->*/}
+            <header class="topo">
+                <img src={logo} alt="Logo Symphonia" class="logo" />
+            </header>
+            <form onSubmit={handleSubmit}>
+                <div className="container">
+                    <div className="card">
+                        <h1 class="titulo">Cadastro</h1>
+
+                        <p class="descricao">
+                            Por favor, escolha um nome de usuário, uma senha e
+                            confirme.
+                        </p>
+
+                        <div className="grupo">
+                            <label>Nome de usuário</label>
+                            <input
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                            />
+                        </div>
+                        <div className="grupo">
+                            <label>Senha</label>
+                            <input
+                                type="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
+
+                        <div class="linha"></div>
+
+                        <div className="grupo">
+                            <label>Confirmar Senha</label>
+                            <input
+                                type="password"
+                                value={confirm}
+                                onChange={(e) => setConfirm(e.target.value)}
+                            />
+                        </div>
+                        {error && <div style={{ color: "red" }}>{error}</div>}
+
+                        <div className="botoes">
+                            <button class="btn-criar" type="submit">
+                                Criar conta
+                            </button>
+                            <button
+                                class="btn-voltar"
+                                type="button"
+                                onClick={() => navigate(-1)}
+                            >
+                                Voltar
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+            *
+        </>
+    );
 }
-
