e711d68bd31cbbe06ce691f5267db80b522f157a
PedroBarbosaIF <pedro.barbosa40@aluno.ifce.edu.br>
2025-12-10 17:43:25 -0300

    estilizaç~çao homepage e pagepost

--- symphonia-project/src/Components/PagePost/PagePost.jsx
+++ symphonia-project/src/Components/PagePost/PagePost.jsx
@@ -1,7 +1,6 @@
 import { useNavigate } from "react-router-dom";
 
 import Stars from "../../assets/stars";
-import "./style.css";
 
 export default function PagePost({ album, avaliation, comment, user, title }) {
    const navigate = useNavigate();
@@ -18,7 +17,58 @@ export default function PagePost({ album, avaliation, comment, user, title }) {
    }
 
    return (
-      <div className="pagepost-div">
+      <>
+         <div className="post">
+            <div className="barra-superior">
+               <img
+                  onClick={() => {
+                     navigate(`/user/${user?._id}`);
+                  }}
+                  className="perfil-post"
+                  src={userphoto}
+                  alt="userphoto"
+               />
+               <span className="avaliacao">Avaliação por</span>
+               <span onClick={()=>{navigate(`/user/${user._id}`)}} className="user">{username}</span>
+               {/*<span className="tempo">Há 15 segundos</span>*/}
+
+               {/*<div className="icones">
+                  <img
+                     src="../assets/favorite.png"
+                     className="icon-heart"
+                     alt="Curtir"
+                  />
+                  <span className="like-count">1</span>
+
+                  <img
+                     src="../assets/comment.png"
+                     className="icon-comment"
+                     alt="Comentar"
+                  />
+               </div>*/}
+            </div>
+
+            <div className="conteudo">
+               <img
+                  className="capa-musica"
+                  src={album?.photo}
+                  alt="album-photo"
+               />
+
+               <div className="info-musica">
+                  <Stars avaliation={avaliation} />
+
+                  <h3 className="titulo">{}</h3>
+                  <p className="banda">{album?.title}</p>
+                  <p className="avaliacao-texto">{comment}</p>
+               </div>
+            </div>
+         </div>
+      </>
+   );
+}
+
+/*<div className="pagepost-div">
          <div className="pagepost-row">
             <img
                onClick={() => {
@@ -48,6 +98,4 @@ export default function PagePost({ album, avaliation, comment, user, title }) {
                <p>{comment}</p>
             </div>
          </div>
-      </div>
-   );
-}
+      </div>*/
--- symphonia-project/src/Pages/HomeFeedPage/HomePage.jsx
+++ symphonia-project/src/Pages/HomeFeedPage/HomePage.jsx
@@ -5,42 +5,67 @@ import api from "../../services/api";
 import PagePost from "../../Components/PagePost/PagePost";
 import { useAuth } from "../../context/AuthContext";
 
+import "./style.css";
+
 //página contendo o feed do site, mostrando posts de usuários variados
 export default function HomePage() {
-    const [posts, setPosts] = useState([]);
-       const { login, user } = useAuth();
+   const [posts, setPosts] = useState([]);
+   const { login, user } = useAuth();
 
-    const navigate = useNavigate();
+   const navigate = useNavigate();
 
-    useEffect(() => {
-        api.get("/posts").then((response) => {
-            console.log(response.data);
-            setPosts(response.data);
-        });
-    }, []);
-    return (
-        <>
+   useEffect(() => {
+      api.get("/posts").then((response) => {
+         console.log(response.data);
+         setPosts(response.data);
+      });
+   }, []);
+
+   if (user != null) {
+      return (
+         <>
+            {/*
             <h1>Página da home/feed</h1>
             <button
-                onClick={() => {
-                    navigate(`/user/${user._id}`);
-                }}
+               onClick={() => {
+                  navigate(`/user/${user._id}`);
+               }}
             >
-                pagina do usuário
+               pagina do usuário
             </button>
+            */}
 
-            {posts.map((post) => {
-                return (
-                    <PagePost
+            <header className="topo">
+               <img src="../assets/logo.png" alt="Logo" className="logo" />
+
+               <input
+                  type="text"
+                  placeholder="Pesquise aqui..."
+                  className="pesquisa"
+               />
+
+               <img onClick={()=>{navigate(`/user/${user._id}`)}} className="perfil-topo" src={user?.photo} alt="user-photo" />
+            </header>
+
+            {/*<button className="btn-criar">Criar Avaliação</button>*/}
+
+            <main className="feed">
+               {posts.map((post) => {
+                  return (
+                     <PagePost
                         key={post._id}
                         user={post.userID}
                         album={post.albumID}
                         avaliation={post.avaliation}
                         title={post.title}
-                        comment = {post.comment}
-                    />
-                );
-            })}
-        </>
-    );
+                        comment={post.comment}
+                     />
+                  );
+               })}
+            </main>
+         </>
+      );
+   } else {
+      navigate("/");
+   }
 }
